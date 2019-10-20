6ef7db7bab470d328b5f0941784d09dd31fef3d7
limira <20672976+limira@users.noreply.github.com>
2019-10-20 10:59:01 +0700

    `use crate::softtube`, not `mod softtube`
    `mod mod_name` use to declare a module.
    `use some_module_name;` bring it into scope

--- src/events.rs
+++ src/events.rs
@@ -1,5 +1,24 @@
-#[path="softtube.rs"]
-mod softtube;
+// You don't need these two lines here:
+// #[path="softtube.rs"]
+// mod softtube;
+
+// And other files need to remove those lines too.
+
+// If you want to bring the module `softtube` into this scope,
+// add the next line:
+
+use crate::softtube;
+// Then, later you just use: `softtube::Search` like this:
+pub fn use_soft_tube() {
+    let _s = softtube::Search::Limit(-1);
+}
+
+// Or 
+use crate::softtube::Search;
+// Then:
+pub fn use_soft_tube_search() {
+    let _s = Search::Limit(-1);
+}
 
 pub fn search_videos(text : String) {
     let search : crate::softtube::Search = match text.len() {
